e9898b7e2dd9bf971996c8a8079806e0bf3819e4
Guilherme Alvarenga de Azevedo <gui.alvarengas234@gmail.com>
2024-04-17 21:53:16 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/br/com/acolherconsultas/acolherconsultas/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.acolherconsultas.acolherconsultas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/app.dart
@@ -0,0 +1,20 @@
+import 'package:acolherconsultas/modules/home/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Acolher Consultas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Sistema Acolher Consultas v1.0'),
+    );
+  }
+}
