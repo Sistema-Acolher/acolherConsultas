289f2074d2f6ab34823be5b8cd66e933a8ef116e
Guilherme Alvarenga de Azevedo <gui.alvarengas234@gmail.com>
2024-05-03 10:22:58 -0300

    feat[HomePage#00]: (Exemplo) Uso das fontes e cores personalizadas

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,5 +1,6 @@
 import 'package:acolherconsultas/modules/home/pages/home.dart';
 import 'package:acolherconsultas/modules/pacientes/controllers/pacientesCadastradosProvider.dart';
+import 'package:acolherconsultas/shared/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         // O theme é uma propriedade que define o tema do aplicativo.
         theme: ThemeData(
           // O colorScheme é uma propriedade que define o esquema de cores do aplicativo.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: amarelo),
           // O useMaterial3 é uma propriedade que define se o aplicativo usa o Material Design 3.
           useMaterial3: true,
         ),
--- lib/modules/home/pages/home.dart
+++ lib/modules/home/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:acolherconsultas/modules/pacientes/pages/cadastroPacienteScreen.dart';
 import 'package:acolherconsultas/modules/pacientes/pages/pacientesCadastradosScreen.dart';
+import 'package:acolherconsultas/shared/colors.dart';
 import 'package:flutter/material.dart';
 
 // A classe HomePage é a classe que representa a página inicial do aplicativo.
@@ -25,7 +26,15 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       // O AppBar é um widget que implementa a barra superior do aplicativo, normalmente com título, botões e ícones de funcionaliades do app.
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          // Exemplo de uso de fonte e cor personalizada.
+          style: const TextStyle(
+            fontFamily: "Montserrat",
+            fontWeight: FontWeight.bold,
+            color: amareloEscuro
+          ),
+        ),
       ),
       // O body é um widget que implementa o corpo do aplicativo, onde são exibidos os conteúdos da página, abaixo do Appbar.
       // O Center é um widget que centraliza o conteúdo do widget filho.
@@ -37,12 +46,26 @@ class _HomePageState extends State<HomePage> {
             TextButton(
               // Em onPressed é definida a ação de navegação para a página de cadastro de pacientes.
               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const CadastroPacienteScreen(title: "Cadastro de Pacientes"))),
-              child: const Text("Cadastro de Pacientes")
+              child: const Text(
+                "Cadastro de Pacientes",
+                // Exemplo de uso de fonte e cor personalizada.
+                style: TextStyle(
+                  fontFamily: "BobbyJonesCondensed",
+                  color: vermelhoEscuro
+                ),
+              )
             ),
             TextButton(
               // Em onPressed é definida a ação de navegação para a página de lista de pacientes cadastrados.
               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const PacientesCadastradosScreen())),
-              child: const Text("Lista de Pacientes Cadastrados")
+              child: const Text(
+                "Lista de Pacientes Cadastrados",
+                // Exemplo de uso de fonte e cor personalizada.
+                style: TextStyle(
+                  fontFamily: "BobbyJonesSoft",
+                  color: verdeEscuro
+                ),
+              )
             ),
           ],
         ),
