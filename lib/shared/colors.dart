844acaf901e1dbbd3cf41097c17540073b1aab66
Guilherme Alvarenga de Azevedo <gui.alvarengas234@gmail.com>
2024-05-03 10:20:48 -0300

    feat[Assets#01]: Adição de cores e fontes personalizadas & Imagens/Gifs

---
+++ lib/shared/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const vermelhoEscuro = Color(0xFFFF3131);
+const vermelho = Color(0xFFE53935);
+const verdeEscuro = Color(0xFF24964F);
+const verde = Color(0xFF7ED957);
+const azulEscuro = Color(0xFF004AAD);
+const azul = Color(0xFF2277AE);
+const amareloEscuro = Color(0xFFF5CB05);
+const amarelo = Color(0xFFFFDE59);
+const preto = Color(0xFF121415);
+const branco = Color(0xFFFCFCFB);
+const cinza = Color(0xFF545454);
+const cinzaClaro = Color(0xFF757575);
