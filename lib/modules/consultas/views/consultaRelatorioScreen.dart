8540e0bdfdebb644ef77f367977addd467f41d6d
Joao <jpmaresp@gmail.com>
2024-05-27 21:13:22 -0300

    [fix#15]: Icones devidamente corrigidos

--- lib/modules/consultas/views/consultaRelatorioScreen.dart
+++ lib/modules/consultas/views/consultaRelatorioScreen.dart
@@ -63,8 +63,6 @@ class _RelatorioCasasScreenState extends State<RelatorioCasasScreen> {
                     options: const ["Servos", "Maria Paola", "Santa Isabel"],
                     label: "Casas",
                     controller: _controllerRelatorioCasas.casas,
-                    icones: const ['src/icons/servos.svg', 'src/icons/mariaPaola.svg', 'src/icons/santaIsabel.svg'],
-                    labelPosition: "right",
                   )
                 ],
               ),
--- lib/shared/components/inputs/inputCheckBox.dart
+++ lib/shared/components/inputs/inputCheckBox.dart
@@ -8,16 +8,12 @@ class InputCheckBoxAcolher extends StatefulWidget {
     required this.options,
     this.label,
     required this.controller,
-    this.icones,
-    required this.labelPosition
   });
 
   // Atributos do componente.
   final List<String> options;
   final String? label;
   final TextEditingController controller;
-  final List<String>? icones;
-  final String labelPosition;
 
   @override
   State<InputCheckBoxAcolher> createState() => _InputCheckBoxAcolherState();
@@ -34,108 +30,82 @@ class _InputCheckBoxAcolherState extends State<InputCheckBoxAcolher> {
     });
   }
 
+  Color checkboxColor(String casa){
+    if (casa == "Maria Paola") {
+      return const Color.fromRGBO(255, 0, 0, 100);
+    }
+    if (casa == "Servos") {
+      return const Color.fromRGBO(34, 119, 174, 100);
+    }
+    if (casa == "Santa Isabel") {
+      return const Color.fromRGBO(120, 177, 88, 100);
+    }
+    return Colors.black;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Column é um widget que organiza os widgets filhos em uma coluna vertical.
-    if (widget.labelPosition == "down"){
-      return Column(
-        children: [
-          ...List.generate(
-          // Definição do tamanho da lista de widgets.
-          widget.options.length,
-          (index) => Column(
-            children: [
-              Checkbox(
-                value: widget.controller.text.contains(widget.options[index]),
-                onChanged: (value) {
-                  setState(() {
-                    // Adiciona ou remove o valor selecionado ao controller de texto.
-                    if (value == true) {
-                      widget.controller.text += "${widget.options[index]},";
-                    } else {
-                      widget.controller.text =
-                          widget.controller.text.replaceAll("${widget.options[index]},", "");
-                    }
-                    validate(); // Chama o método de validação quando o valor muda
-                  });
-                },
-              ),
-              Text(
-                widget.options[index],
-                style: const TextStyle(
-                  fontFamily: 'BobbyJonesSoft',
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        "${widget.label}:",
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
-        ],
-      );
-    }
-    else{
-      return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "${widget.label}:",
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        // List.generate é uma função que gera uma lista de widgets a partir de uma lista de dados.
-        ...List.generate(
-          // Definição do tamanho da lista de widgets.
-          widget.options.length,
-          (index) => Row(
-            children: [
-              Checkbox(
-                value: widget.controller.text.contains(widget.options[index]),
-                onChanged: (value) {
-                  setState(() {
-                    // Adiciona ou remove o valor selecionado ao controller de texto.
-                    if (value == true) {
-                      widget.controller.text += "${widget.options[index]},";
-                    } else {
-                      widget.controller.text =
-                          widget.controller.text.replaceAll("${widget.options[index]},", "");
-                    }
-                    validate(); // Chama o método de validação quando o valor muda
-                  });
-                },
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.options[index],
-                    style: const TextStyle(
-                      fontFamily: 'BobbyJonesSoft',
-                      fontSize: 20,
-                    ),
-                  ),
-                  const SizedBox(width: 4.0),
-                  SvgPicture.asset(
-                    widget.icones![index], // Usando o caminho do ícone da lista de caminhos
-                    width: 24.0,
-                    height: 24.0,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        if (!_isValid)
-          const Text(
-            'Selecione pelo menos uma opção.',
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 13,
+      ),
+      // List.generate é uma função que gera uma lista de widgets a partir de uma lista de dados.
+      ...List.generate(
+        // Definição do tamanho da lista de widgets.
+        widget.options.length,
+        (index) => Row(
+          children: [
+            Checkbox(
+              value: widget.controller.text.contains(widget.options[index]),
+              onChanged: (value) {
+                setState(() {
+                  // Adiciona ou remove o valor selecionado ao controller de texto.
+                  if (value == true) {
+                    widget.controller.text += "${widget.options[index]},";
+                  } else {
+                    widget.controller.text =
+                        widget.controller.text.replaceAll("${widget.options[index]},", "");
+                  }
+                  validate(); // Chama o método de validação quando o valor muda
+                });
+              },
             ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.options[index],
+                  style: const TextStyle(
+                    fontFamily: 'BobbyJonesSoft',
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(width: 4.0),
+                Icon(
+                  size: 30,
+                  Icons.cottage_outlined,
+                  color: checkboxColor(widget.options[index]),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      if (!_isValid)
+        const Text(
+          'Selecione pelo menos uma opção.',
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 13,
           ),
+        ),
       ],
-      );
-    }    
-  }
+    );
+  }    
 }
